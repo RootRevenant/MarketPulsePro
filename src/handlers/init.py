fac5f45eb94532703a077d81741347a9ccaed783
RootRevenant <herlozen62@gmail.com>
2025-12-27 17:41:57 +0330

    Update and rename __init__.py to init.py

---
+++ src/handlers/init.py
@@ -0,0 +1,16 @@
+# Handler modules
+from . import (
+    user_handlers,
+    admin_handlers,
+    price_handlers,
+    news_handlers,
+    callback_handlers
+)
+
+__all__ = [
+    'user_handlers',
+    'admin_handlers',
+    'price_handlers',
+    'news_handlers',
+    'callback_handlers'
+]
